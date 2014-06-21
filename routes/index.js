904c78b1ea39052f06f0db24c47f4477a21e4d14
monokeroslun <lun2baobao@gmail.com>
2014-06-21 23:12:00 +0800

    add

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+/*
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Express' });
+});
+*/
+
+router.get('/', function(req, res) {
+    res.sendfile('views/demo.html');
+});
+
+module.exports = router;
